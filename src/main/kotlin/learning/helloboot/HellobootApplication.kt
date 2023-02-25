aaede9fdd8d031c32613b05452cf9c46af226527
codingmate <hsykys0728@gmail.com>
2023-02-25 19:17:57 +0900

    main > Create Servlet Container & print query param Map

--- src/main/kotlin/learning/helloboot/HellobootApplication.kt
+++ src/main/kotlin/learning/helloboot/HellobootApplication.kt
@@ -1,5 +1,51 @@
 package learning.helloboot
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.ServletContextInitializer
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
 fun main(args: Array<String>) {
-    println("Containerless Standalone Application")
+    // ServletWebServerFactory : Servlet Container 추상화
+    val servletWebServerFactory: ServletWebServerFactory
+    // Tomcat의 Servlet Container 생성 팩토리로 간편하게 인스턴스 생성
+            = TomcatServletWebServerFactory()
+    // TomcatFactory 생성된 컨테이너의 WebServer를 가져옴
+    servletWebServerFactory.getWebServer(
+
+        ServletContextInitializer { // 재사용하지 않고, 이 곳에서만 사용할 것이므로 lambda 사용
+            // servlet container 실행자
+            it.addServlet("hello", object : HttpServlet() {
+                override fun service(req: HttpServletRequest, res: HttpServletResponse) {
+                    val paramMap = req.parameterMap
+                    println(paramMap.keys.joinToString())
+
+                    val values = StringBuilder()
+                    for ( key in paramMap.keys )
+                        for ( value in paramMap[key]!! )
+                            values.append("value : ${value}\n")
+                    println(values)
+
+
+
+
+                // status code
+                    //res.status = 200
+                    res.status = HttpStatus.OK.value()
+                // header : contents-type
+                    //res.setHeader("Content-Type", "text/plain")
+                    res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
+                // body
+                    res.writer.println("Hello!, input : ${values}")
+                }
+            }).addMapping("/hello")
+        } // functional     interface : only one method
+    )
+        .start() // Tomcat을 실행
 }
