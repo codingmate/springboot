716b509e1cd816ddc9be058027707c94accbc032
codingmate <hsykys0728@gmail.com>
2023-02-25 16:22:19 +0900

    main > from printn() app -> to SpringBoot Application

--- src/main/kotlin/learning/helloboot/HellobootApplication.kt
+++ src/main/kotlin/learning/helloboot/HellobootApplication.kt
@@ -1,11 +1,5 @@
 package learning.helloboot
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class HellobootApplication
-
 fun main(args: Array<String>) {
-    runApplication<HellobootApplication>(*args)
+    println("Containerless Standalone Application")
 }
