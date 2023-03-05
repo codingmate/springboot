5119653d47b6ccc94b26d9d0a68da7410082dc66
codingmate <hsykys0728@gmail.com>
2023-03-05 18:42:35 +0900

    Use Servelet Container & Spring Container(application context)

---
+++ src/main/kotlin/learning/helloboot/SpringContainerApplication.kt
@@ -0,0 +1,45 @@
+package learning.helloboot
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.ServletContextInitializer
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.context.support.registerBean
+import org.springframework.http.MediaType
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+fun main(args: Array<String>) {
+    val applicationContext = GenericApplicationContext()
+    applicationContext.apply{
+        this.registerBean<HelloController>()
+        this.refresh()
+    }
+
+
+
+
+    val servletWebServerFactory: ServletWebServerFactory = TomcatServletWebServerFactory()
+
+    servletWebServerFactory.getWebServer(
+        ServletContextInitializer { // 재사용하지 않고, 이 곳에서만 사용할 것이므로 lambda 사용
+            it.addServlet("hello", object : HttpServlet() {
+                override fun service(req: HttpServletRequest, res: HttpServletResponse) {
+                    val name = "Tomson"
+                    val helloController = applicationContext.getBean(HelloController::class.java)
+                    val ret = helloController.hello(name)
+
+                    res.contentType = MediaType.TEXT_PLAIN_VALUE
+                    res.writer.println(ret)
+                }
+            }).addMapping("/hello")
+        }
+    )
+        .start() // Tomcat을 실행
+
+}
+
+
